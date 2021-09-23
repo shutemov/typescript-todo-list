8b1a1906b7afa49f3140fcba2247d4be04bd2311
Shutemov <shutemov1997@mail.ru>
2021-09-23 09:40:45 +0300

    Создает index.ts
    Тестовое содержание для проверки компиляции

---
+++ src/scripts/index.ts
@@ -0,0 +1,12 @@
+const helloWorld: string = "hello world"
+let test: number = 1
+let test1: number = 1
+let test2: number = 1
+let test3: number = 1
+
+class testClass {
+    constructor({a: number, b: string}) {
+    }
+}
+
+console.log(helloWorld, test, testClass)
