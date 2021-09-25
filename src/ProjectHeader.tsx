8ea2937623ed69e91323104dd0bb215a4c1f67fe
Shutemov <shutemov1997@mail.ru>
2021-09-25 17:08:23 +0300

    Создает компонент ProjectHeader

---
+++ src/ProjectHeader.tsx
@@ -0,0 +1,10 @@
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+
+export const ProjectHeader = () => {
+    return (
+        <Typography variant="h3" align="left">
+            Project name
+        </Typography>
+    )
+}
