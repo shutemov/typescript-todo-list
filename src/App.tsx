f54ea9d60c75438043ca5ec5b6383af0193875f4
Shutemov <shutemov1997@mail.ru>
2021-09-25 14:20:00 +0300

    Убирает дефолтный шаблон App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
+import Paper from '@mui/material/Paper';
+import Stack from '@material-ui/core/Stack';
+import {styled} from '@mui/material/styles';
+import Header from "./Header";
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Project from "./Project";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Project/>
+        </div>
+    );
 }
 
 export default App;
