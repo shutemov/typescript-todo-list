4ac2ec53157c363827c5ab1bc1546b7304a0641c
Shutemov <shutemov1997@mail.ru>
2021-09-26 17:22:44 +0300

    Выносит часть компонента TaskList в компонент Task

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -1,27 +1,15 @@
+import React from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Checkbox from "@mui/material/Checkbox";
+import {Task} from './Task'
 
 export default function TaskList() {
     return (
         <List>
             <ListItem disablePadding>
                 <ListItemButton>
-                    <ListItemIcon>
-                        <Checkbox defaultChecked/>
-                    </ListItemIcon>
-                    <ListItemText primary="Drafts"/>
-                </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-                <ListItemButton>
-                    <ListItemIcon>
-                        <Checkbox defaultChecked/>
-                    </ListItemIcon>
-                    <ListItemText primary="Drafts"/>
+                    <Task/>
                 </ListItemButton>
             </ListItem>
         </List>
