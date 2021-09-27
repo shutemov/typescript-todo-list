49f92d930f719d95072450b03a333249ae59cec1
Shutemov <shutemov1997@mail.ru>
2021-09-27 14:28:09 +0300

    Добавляет основной шаблон ProjectsPage

--- src/ProjectsPage.tsx
+++ src/ProjectsPage.tsx
@@ -97,4 +97,67 @@ export const ProjectsPage = () => {
         setOpen(false);
     };
 
+    return (
+        <Box sx={{display: 'flex'}}>
+            <CssBaseline/>
+            <AppBar position="fixed" open={open}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{mr: 2, ...(open && {display: 'none'})}}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        Projects
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="persistent"
+                anchor="left"
+                open={open}
+            >
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider/>
+                <List>
+                    {projects.map(({id, name}) => (
+                        <ListItem button key={id} onClick={() => setProjectId(id)}>
+                            <ListItemText primary={name}/>
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider/>
+                <List>
+                    {/*{['All mail', 'Trash', 'Spam'].map((text, index) => (*/}
+                    {/*    <ListItem button key={text}>*/}
+                    {/*        <ListItemIcon>*/}
+                    {/*            {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}*/}
+                    {/*        </ListItemIcon>*/}
+                    {/*        <ListItemText primary={text}/>*/}
+                    {/*    </ListItem>*/}
+                    {/*))}*/}
+                </List>
+            </Drawer>
+            <Main open={open}>
+                <DrawerHeader/>
+                <Project id={projectId}/>
+            </Main>
+        </Box>
+    );
 }
