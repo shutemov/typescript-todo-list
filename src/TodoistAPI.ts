8ee2e28e016b8e40fbbb706d420b4b14a147af06
Shutemov <shutemov1997@mail.ru>
2021-09-27 14:22:44 +0300

    Добавляет type Project в TodoistAPI

--- src/TodoistAPI.ts
+++ src/TodoistAPI.ts
@@ -1,2 +1,18 @@
 import {apiConfig} from "./apiconfig"
 
+const {access_key} = apiConfig
+
+type Project = {
+    id: number,
+    name: string,
+    comment_count: number,
+    order: number,
+    color: number,
+    shared: boolean,
+    sync_id: number,
+    favorite: boolean,
+    parent_id?: number,
+    team_inbox: boolean,
+    inbox_project: boolean,
+    url: string
+}
