343f90057e14e472ca728b3695c5375b88b108d7
Shutemov <shutemov1997@mail.ru>
2021-09-27 14:23:02 +0300

    Добавляет получение всех проектов в TodoistAPI

--- src/TodoistAPI.ts
+++ src/TodoistAPI.ts
@@ -16,3 +16,14 @@ type Project = {
     inbox_project: boolean,
     url: string
 }
+
+
+export const getProjects = async () => {
+    const url: string = `https://api.todoist.com/rest/v1/projects`
+    const headers: HeadersInit = new Headers();
+    headers.set('Authorization', `Bearer ${apiConfig.access_key}`);
+
+    const response = await fetch(url, {headers})
+    const data: Array<Project> = await response.json()
+    return data
+}
