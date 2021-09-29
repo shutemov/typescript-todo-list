74bf64695b0a2cd62f262281389fafba3f4aa63a
Shutemov <shutemov1997@mail.ru>
2021-09-29 10:05:44 +0300

    Добавляет шаблон в SectionOptionPopper

--- src/SectionOptionPopper.tsx
+++ src/SectionOptionPopper.tsx
@@ -23,4 +23,50 @@ export const SectionOptionPopper = () => {
     const open = Boolean(anchorEl);
     const id = open ? 'simple-popper' : undefined;
 
+    return (
+        <div>
+            <IconButton aria-describedby={id} aria-label="settings" onClick={handleClick}>
+                <MoreVertIcon/>
+            </IconButton>
+            <Popper id={id} open={open} anchorEl={anchorEl}>
+                <Box sx={{border: 1, p: 1, bgcolor: 'background.paper'}}>
+                    <List
+                        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+                        component="nav"
+                    >
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ModeEditOutlineOutlinedIcon/>
+                            </ListItemIcon>
+                            <ListItemText primary="Edit section" />
+                        </ListItemButton>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ArrowForwardOutlinedIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Move section" />
+                        </ListItemButton>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ArchiveTwoToneIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Archive" />
+                        </ListItemButton>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <FileCopyTwoToneIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Duplicate" />
+                        </ListItemButton>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <DeleteTwoToneIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Delete section" />
+                        </ListItemButton>
+                    </List>
+                </Box>
+            </Popper>
+        </div>
+    );
 }
