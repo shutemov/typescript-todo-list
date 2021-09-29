8c8467f260f9a5ea72431835a31dcf3583898e4c
Shutemov <shutemov1997@mail.ru>
2021-09-29 10:04:35 +0300

    Создает компонент SectionOptionPopper
    Отображает в Popper действия над секцией

---
+++ src/SectionOptionPopper.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Popper from '@mui/material/Popper';
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import IconButton from "@mui/material/IconButton";
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
+import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
+import ArchiveTwoToneIcon from '@mui/icons-material/ArchiveTwoTone';
+import FileCopyTwoToneIcon from '@mui/icons-material/FileCopyTwoTone';
+import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
+
+export const SectionOptionPopper = () => {
+}
